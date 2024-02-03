00d597d93a720f73f4d3d4281dae5d2b2bc4472d
Harsh Tomar <harsh@Harshs-MacBook-Air.local>
2024-02-03 15:32:43 +0530

    Add color

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Convertor from './Components/Convertor/Convertor';
-import Header from './Components/Header/Header';
+import "./App.css";
+import Convertor from "./Components/Convertor/Convertor";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
-      <Header/>
+      <Header />
 
-      <Convertor/>
+      <h1 className="App__title">Ht Currency Convertor</h1>
 
+      <Convertor />
     </div>
   );
 }
--- src/Components/Convertor/Convertor.jsx
+++ src/Components/Convertor/Convertor.jsx
@@ -1,28 +1,27 @@
 import "./Convertor.css";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ConversionResult from "../Result/ConversionResult";
 
 export default function Convertor() {
-
     const [currency, setCurrency] = useState([]);
-    const [amount, setAmount] = useState('0');
-    const [from, setFrom] = useState('USD');
-    const [to, setTo] = useState('INR');
+    const [amount, setAmount] = useState("0");
+    const [from, setFrom] = useState("USD");
+    const [to, setTo] = useState("INR");
     const [conversion, setConversion] = useState(0);
     const [isResult, setIsResult] = useState(false);
 
     useEffect(() => {
-
         const fetch = async () => {
             const options = {
-                method: 'GET',
-                url: 'https://currency-exchange.p.rapidapi.com/listquotes',
+                method: "GET",
+                url: "https://currency-exchange.p.rapidapi.com/listquotes",
                 headers: {
-                    'X-RapidAPI-Key': '6fc01cf93fmsh8ce7fac572c1e77p112105jsn04f92dc6d157',
-                    'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-                }
+                    "X-RapidAPI-Key":
+                        "6fc01cf93fmsh8ce7fac572c1e77p112105jsn04f92dc6d157",
+                    "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+                },
             };
 
             try {
@@ -32,7 +31,7 @@ export default function Convertor() {
             } catch (error) {
                 console.error(error);
             }
-        }
+        };
 
         fetch();
     }, []);
@@ -41,41 +40,40 @@ export default function Convertor() {
         let input = event.target.value;
 
         if (!isNaN(input)) {
-
-            if (amount.length === 1 && amount === '0') { // to avoid 035
-                console.log("here")
+            if (amount.length === 1 && amount === "0") {
+                // to avoid 035
+                console.log("here");
                 setAmount(input.substr(1));
-            }
-            else {
+            } else {
                 setAmount(input);
             }
         }
-    }
+    };
 
     const changeFrom = (event) => {
         setFrom(event.target.value);
         setIsResult(false);
-    }
+    };
 
     const changeTo = (event) => {
         setTo(event.target.value);
         setIsResult(false);
-    }
+    };
 
     const convert = async () => {
         console.log(amount);
         const options = {
-            method: 'GET',
-            url: 'https://currency-exchange.p.rapidapi.com/exchange',
+            method: "GET",
+            url: "https://currency-exchange.p.rapidapi.com/exchange",
             params: {
                 to,
                 from,
-                q: amount
+                q: amount,
             },
             headers: {
-                'X-RapidAPI-Key': '6fc01cf93fmsh8ce7fac572c1e77p112105jsn04f92dc6d157',
-                'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-            }
+                "X-RapidAPI-Key": "6fc01cf93fmsh8ce7fac572c1e77p112105jsn04f92dc6d157",
+                "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+            },
         };
 
         try {
@@ -86,67 +84,58 @@ export default function Convertor() {
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     return (
-        <article className='convertor'>
-
-            <section className='convertor__main'>
-
+        <article className="convertor">
+            <section className="convertor__main">
                 <label>
-                    Amount
+                    <span>Amount</span>
                     <input onChange={changeAmount} value={amount} />
-                    {
-                        amount === '' &&
-                        <span className="error">
-                            Please enter a valid amount
-                        </span>
-                    }
+                    {amount === "" && (
+                        <span className="error">Please enter a valid amount</span>
+                    )}
                 </label>
 
                 <label>
-                    From
+                    <span>From</span>
                     <select value={from} onChange={changeFrom}>
                         {/* <option>Select currency</option> */}
-                        {
-                            currency.map((currency, index) => (
-                                <option key={currency + index} value={currency}>
-                                    {currency}
-                                </option>
-                            ))
-                        }
+                        {currency.map((currency, index) => (
+                            <option key={currency + index} value={currency}>
+                                {currency}
+                            </option>
+                        ))}
                     </select>
                 </label>
 
                 <label>
-                    To
+                    <span>To</span>
                     <select value={to} onChange={changeTo}>
                         {/* <option>Select currency</option> */}
-                        {
-                            currency.map((currency, index) => (
-                                <option key={currency + index} value={currency}>
-                                    {currency}
-                                </option>
-                            ))
-                        }
+                        {currency.map((currency, index) => (
+                            <option key={currency + index} value={currency}>
+                                {currency}
+                            </option>
+                        ))}
                     </select>
                 </label>
-
             </section>
 
-            <section className='convertor__footer'>
-                {
-                    isResult
-                        ? <ConversionResult from={from} to={to} amount={amount} conversion={conversion} />
-                        :
-
-                        <button className='convertor__btn' onClick={convert}>
-                            Convert
-                        </button>
-                }
-
+            <section className="convertor__footer">
+                {isResult ? (
+                    <ConversionResult
+                        from={from}
+                        to={to}
+                        amount={amount}
+                        conversion={conversion}
+                    />
+                ) : (
+                    <button className="convertor__btn" onClick={convert}>
+                        Convert
+                    </button>
+                )}
             </section>
-
         </article>
-    )
+    );
 }
